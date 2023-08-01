7e767ca93b437728e51a589ab17c1abb1fec1bfd
jvonappen <99298095+jvonappen@users.noreply.github.com>
2023-08-01 13:38:04 +0930

    Police AI
    - Added Behavior Tree and Nodes
    - Using Piggy Cop
    - Duplicated Scene for testing

---
+++ Assets/Jacob.Testing/AI/Nodes/ChaseNode.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaseNode : Node
+{
+    private Transform target;
+    private NavMeshAgent agent;
+
+    public ChaseNode(Transform target, NavMeshAgent agent)
+    {
+        this.target = target;
+        this.agent = agent;
+    }
+
+    public override NodeState Evaluate()
+    {
+        float distance = Vector3.Distance(target.position, agent.transform.position);
+        if (distance > 0.2f)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(target.position);
+            Debug.Log("isChasing State");
+            return NodeState.RUNNING;
+        }
+        else
+        {
+            agent.isStopped = true;
+            Debug.Log("Chase complete");
+            return NodeState.PASS;
+        }
+
+    }
+}
---
+++ Assets/Jacob.Testing/AI/PoliceAI.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using TMPro;
+using UnityEditor;
+
+public class PoliceAI : MonoBehaviour
+{
+    //[Header("Alert")]
+    [Tooltip("Distance the AI will chase away the player.")]
+    [SerializeField] private float chasingRange = 3;
+    [Tooltip("Distance the AI will become alert to the player.")]
+    [SerializeField] private float awareRange = 5;
+    //[Tooltip("The Player")]
+    [SerializeField] private Transform playerTransform;
+
+    //[Header("Wander")]
+    [Tooltip("The center of the wandering radius. Use a fixed object to keep the wander area fixed. Use the animal itself, to have complete free roam.")]
+    [SerializeField] private Transform wanderTransform;
+    [Tooltip("How far the AI will wander from the wander tranform.")]
+    [SerializeField] private float wanderRange = 25;
+
+    [Header("UI Notification")]
+    [Tooltip("Optional GUI to be displayed above the AI.")]
+    [SerializeField] private bool usingGUI = true;
+    [SerializeField] private GameObject alertUI;
+    [SerializeField] private GameObject chaseUI;
+
+    private NavMeshAgent agent;
+    private Node topNode;
+
+    [Tooltip("The speed the animal moves. If left at 0, the speed will default to the navMesh speed.")]
+    [SerializeField] private float normalMovementSpeed = 3.5f;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        
+        SetMovementSpeed();
+        BuildAnimalBehaviourTree();
+        //ResetChecks();
+    }
+
+
+
+    private void BuildAnimalBehaviourTree() /* Tree works from bottom up */
+    {
+        //Wander
+        WanderNode wanderNode = new WanderNode(wanderTransform, agent, wanderRange); //use 'transform' to set center on agent.
+
+        //Alert and Chase
+        ChaseNode chaseNode = new ChaseNode(playerTransform, agent);
+        AlertNode awareNode = new AlertNode(agent, this, playerTransform);
+        RangeNode chasingRangeNode = new RangeNode(chasingRange, playerTransform, transform, chaseUI);
+        RangeNode awareRangeNode = new RangeNode(awareRange, playerTransform, transform, alertUI);
+        Sequence chaseSequence = new Sequence(new List<Node> { chasingRangeNode, chaseNode, });
+        Sequence awareSequence = new Sequence(new List<Node> { awareRangeNode, awareNode });
+
+        //Top Selector
+        topNode = new Selector(new List<Node> { chaseSequence, awareSequence, wanderNode });
+        //--------------------------------------------------------------------------------------------------------------
+    }
+
+    private void Update()
+    {
+        topNode.Evaluate();
+    }
+
+    private void SetMovementSpeed()
+    {
+        if (normalMovementSpeed > 0)
+        {
+            agent.speed = normalMovementSpeed;
+        }
+        else { normalMovementSpeed = agent.speed; }
+
+    }
+}
