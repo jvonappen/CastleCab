cb5e98832f44296715cb4e42735bb3003d29ba11
LukeStanbridge <99298136+LukeStanbridge@users.noreply.github.com>
2023-08-07 16:41:29 +0930

    player wagon

--- Assets/LukeTesting/Scripts/PlayerInput.cs
+++ Assets/LukeTesting/Scripts/PlayerInput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +11,28 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private float _steeringInput;
     [SerializeField] private float _jumpInput;
 
+<<<<<<< Updated upstream
     [SerializeField] private float _moveAcceleration = 50f;
     [SerializeField] private float _maxSpeed = 15f;
     [SerializeField] private float _drag = 0.98f;
     [SerializeField] private float _steerAngle = 5f;
     [SerializeField] private float _traction = 1f;
     [SerializeField] private float _reverseSpeed = 5f;
+=======
+    [SerializeField] private Rigidbody _sphereRB;
+    [SerializeField] private float _forwardAcceleration = 8f;
+    [SerializeField] private float _reverseAcceleration = 4f;
+    [SerializeField] private float _maxSpeed = 50f;
+    [SerializeField] private float _turnStrength = 180f;
+    [SerializeField] private float _gravityForce = 10f;
+    [SerializeField] private float _dragOnGround = 3f;
+    [SerializeField] private float maxTippingAngle = 45f;
+    [SerializeField] private LayerMask _whatIsGround;
+    [SerializeField] private Transform _groundRayPoint;
+    [SerializeField] private float _groundRayLength = 0.5f;
+    private bool _grounded;
+    private float _speedInput;
+>>>>>>> Stashed changes
     
     private Vector3 _moveSpeed;
 
@@ -30,7 +47,11 @@ public class PlayerInput : MonoBehaviour
     private void Awake()
     {
         _playerInput = new PlayerControls();
+<<<<<<< Updated upstream
         _playerRb = GetComponent<Rigidbody>();
+=======
+        //_sphereRB.transform.parent = null;
+>>>>>>> Stashed changes
     }
 
     private void OnEnable()
@@ -49,8 +70,9 @@ public class PlayerInput : MonoBehaviour
         _playerInput.Disable();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
+<<<<<<< Updated upstream
         _grounded = false;
         RaycastHit hit;
 
@@ -82,6 +104,54 @@ public class PlayerInput : MonoBehaviour
             //Traction
             _moveSpeed = Vector3.Lerp(_moveSpeed.normalized, transform.forward, _traction * Time.deltaTime) * _moveSpeed.magnitude;
         }
+=======
+        //Moving
+        _speedInput = 0f;
+        _speedInput = _accelerationInput > 0 ? _forwardAcceleration : _reverseAcceleration;
+        _speedInput *= 1000f * _accelerationInput;
+
+        //Steering
+        if (_grounded)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, _steeringInput * _turnStrength * Time.deltaTime * _accelerationInput, 0f));
+        }
+
+        //Update positon
+        //transform.position = _sphereRB.transform.position;
+        Debug.DrawRay(_groundRayPoint.position, -Vector3.up, Color.red);
+    }
+
+    private void FixedUpdate()
+    {
+        _grounded = false;
+        RaycastHit hit;
+
+        if(Physics.Raycast(_groundRayPoint.position, Vector3.down, out hit, _groundRayLength, _whatIsGround))
+        {
+            _grounded = true;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation, Time.fixedDeltaTime * 10.0f);
+        }
+
+        if(_grounded)//control car on ground
+        {
+            _sphereRB.drag = _dragOnGround;
+            _sphereRB.AddForce(transform.forward * _speedInput);
+            _groundRayLength = 0.5f;
+        }
+        else//add gravity when in air
+        {
+            _sphereRB.drag = 0.0f;
+            _sphereRB.AddForce(Vector3.up * -_gravityForce * 100f);
+            _groundRayLength = 1.5f;
+        }
+
+        float angle = Vector3.Angle(transform.up, Vector3.up);
+        if (angle > maxTippingAngle)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(transform.up, Vector3.up), Mathf.InverseLerp(angle, 0, maxTippingAngle));
+        }
+
+>>>>>>> Stashed changes
     }
 
     private void OnAccelerate(InputAction.CallbackContext value)
