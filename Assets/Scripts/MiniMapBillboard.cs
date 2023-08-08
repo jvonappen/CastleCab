f3db2bc76b2426e6448ae465fa953eca4e12e010
jvonappen <99298095+jvonappen@users.noreply.github.com>
2023-08-08 15:17:02 +0930

    MiniMap
    Added MiniMap
    Water Shader
    Tweaks and changes to UI

---
+++ Assets/Scripts/MiniMapBillboard.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapBillboard : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+    void LateUpdate()
+    {
+        //transform.LookAt(MiniMapCam.miniMapCamera.transform);
+        //transform.Rotate(0, 0, 180);
+        //transform.transform.position.x
+    }
+}
---
+++ Assets/Scripts/MiniMapCam.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCam : MonoBehaviour
+{
+    public static Camera miniMapCamera;
+
+    [SerializeField] private Camera m_Camera;
+    [SerializeField] private GameObject player;
+
+    private Vector3 pos;
+
+    private void Awake()
+    {
+        miniMapCamera = m_Camera;
+    }
+
+    private void LateUpdate()
+    {
+        pos = new Vector3(player.transform.position.x, m_Camera.transform.position.y, player.transform.position.z);
+        m_Camera.transform.position = pos;    
+    }
+
+}
