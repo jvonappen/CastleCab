8b2d5c82f45bd9a63b4d93fa48c923b29fb7c858
jvonappen <jacobe.vonappen@gmail.com>
2023-08-16 12:51:07 +0930

    Working on Dialogue
    -Created additional tags for TMPro for text effects
    - Added scripts for easy Villager dialogue creation
    ***Still a work in progress

---
+++ Assets/Jacob.Testing/Dialogue/TestingTextParser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TMP_Animated))]
+public class TestingTextParser : MonoBehaviour
+{
+
+    [TextArea] public string masterText;
+    //private TMP_Animated animatedText;
+
+    private TextMeshProUGUI animatedText;
+    private void Awake()
+    {
+        animatedText = GetComponent<TMP_Animated>();
+    }
+
+}
---
+++ Assets/Jacob.Testing/Dialogue/Villager/VillagerData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Villager", menuName = "Villager")]
+public class VillagerData : ScriptableObject
+{
+    [SerializeField] private string villagerName;
+    public Color villagerColor;
+    public Color villagerNameColor;
+    public DialogueData dialogue;
+}
