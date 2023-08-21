0a97b6d47efea71323efab1c9e547232a097038c
jvonappen <99298095+jvonappen@users.noreply.github.com>
2023-08-21 13:29:05 +0930

    Dialogue UI
    -Added UI place holder
    - Changes to Dialogue Triggers

--- Assets/Jacob.Testing/Dialogue/DialogueTrigger.cs
+++ Assets/Jacob.Testing/Dialogue/DialogueTrigger.cs
@@ -6,54 +6,56 @@ using Cinemachine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    //private InterfaceManager ui;
-    //private VillagerScript currentVillager;
+    private InterfaceManager ui;
+    private VillagerScript currentVillager;
     //private MovementInput movement;
-    //public CinemachineTargetGroup targetGroup;
+    public CinemachineTargetGroup targetGroup;
 
-    //[Space]
+    [Space]
 
-    //[Header("Post Processing")]
-    //public Volume dialogueDof;
+    [Header("Post Processing")]
+    public Volume dialogueDof;
 
-    //void Start()
-    //{
-    //    ui = InterfaceManager.instance;
-    //    movement = GetComponent<MovementInput>();
-    //}
+    void Start()
+    {
+        ui = InterfaceManager.instance;
+        //movement = GetComponent<MovementInput>();
+    }
 
-    //void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.Space) && !ui.inDialogue && currentVillager != null)
-    //    {
-    //        targetGroup.m_Targets[1].target = currentVillager.transform;
-    //        movement.active = false;
-    //        ui.SetCharNameAndColor();
-    //        ui.inDialogue = true;
-    //        ui.CameraChange(true);
-    //        ui.ClearText();
-    //        ui.FadeUI(true, .2f, .65f);
-    //        currentVillager.TurnToPlayer(transform.position);
-    //    }
-    //}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !ui.inDialogue && currentVillager != null)
+        {
+            targetGroup.m_Targets[1].target = currentVillager.transform;
+            //movement.active = false;
+            ui.SetCharNameAndColor();
+            ui.inDialogue = true;
+            ui.CameraChange(true);
+            ui.ClearText();
+            ui.FadeUI(true, .2f, .65f);
+            currentVillager.TurnToPlayer(transform.position);
+
+            Debug.Log("SpaceBar");
+        }
+    }
 
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.CompareTag("Villager"))
-    //    {
-    //        currentVillager = other.GetComponent<VillagerScript>();
-    //        ui.currentVillager = currentVillager;
-    //    }
-    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Villager"))
+        {
+            currentVillager = other.GetComponent<VillagerScript>();
+            ui.currentVillager = currentVillager;
+        }
+    }
 
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (other.CompareTag("Villager"))
-    //    {
-    //        currentVillager = null;
-    //        ui.currentVillager = currentVillager;
-    //    }
-    //}
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Villager"))
+        {
+            currentVillager = null;
+            ui.currentVillager = currentVillager;
+        }
+    }
 
 }
