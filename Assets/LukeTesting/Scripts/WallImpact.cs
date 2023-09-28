087bb5be56390514c24a5bda53cf47b902bbf48a
LukeStanbridge <bridge48@hotmail.com.au>
2023-09-28 17:16:30 +0930

    fixed particles, wagon pos and pig impact with wham

--- Assets/LukeTesting/Scripts/WallImpact.cs
+++ Assets/LukeTesting/Scripts/WallImpact.cs
@@ -7,11 +7,11 @@ public class WallImpact : MonoBehaviour
 {
     [SerializeField] private SoundManager _soundManager;
     [SerializeField] private ParticleSystem _wallImpact;
-    [SerializeField] private ParticleSystem _wham;
+    //[SerializeField] private ParticleSystem _wham;
     [SerializeField] private float _hitForce = 100;
     [SerializeField] private Rigidbody _playerRB;
-    [SerializeField] private Transform _whamPos;
-    [SerializeField] private List<GameObject> _whams;
+    //[SerializeField] private Transform _whamPos;
+    //[SerializeField] private List<GameObject> _whams;
 
     private void Awake()
     {
@@ -25,21 +25,21 @@ public class WallImpact : MonoBehaviour
         {
             _soundManager.Play("WallImpact");
             ParticleSystem impact = Instantiate(_wallImpact, this.transform); //destroy by itself
-            ParticleSystem wham = Instantiate(_wham, _whamPos);
-            _whams.Add(wham.gameObject); //add to list to be destroyed
+            //ParticleSystem wham = Instantiate(_wham, _whamPos);
+            //_whams.Add(wham.gameObject); //add to list to be destroyed
             _playerRB.AddForce(collision.contacts[0].normal * _hitForce, ForceMode.Impulse); //knockback player
         }
     }
 
-    private void Update()
-    {
-        if (_whams.Count > 0) //destroy instantiated whams
-        {
-            foreach (var wham in _whams.ToList())
-            {
-                Destroy(wham, 1);
-                if (wham == null) _whams.Remove(wham);
-            }
-        }
-    }
+    //private void Update()
+    //{
+    //    if (_whams.Count > 0) //destroy instantiated whams
+    //    {
+    //        foreach (var wham in _whams.ToList())
+    //        {
+    //            Destroy(wham, 1);
+    //            if (wham == null) _whams.Remove(wham);
+    //        }
+    //    }
+    //}
 }
