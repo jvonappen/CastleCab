4589cb9e9ab67f2fa0dbafac9284e897cfc82b91
LukeStanbridge <99298136+LukeStanbridge@users.noreply.github.com>
2023-10-10 13:30:42 +1030

    added collectable letter system

---
+++ Assets/LukeTesting/Scripts/Collectable.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    [field: SerializeField] public char _collectibleLetter { get; private set; }
+    private DisplayCollectables _collectables;
+
+    private void Awake()
+    {
+        _collectables = FindObjectOfType<DisplayCollectables>();
+        this.gameObject.name = _collectibleLetter.ToString();
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0, 0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Hit Letter");
+            _collectables.SetCollectableActive(this);
+            Destroy(gameObject);
+        }
+    }
+}
