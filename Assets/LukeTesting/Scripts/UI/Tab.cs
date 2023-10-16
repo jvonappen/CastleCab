4e15c63f6931fda8019b892c6a50089221ae78b8
LukeStanbridge <99298136+LukeStanbridge@users.noreply.github.com>
2023-10-16 16:37:06 +1030

    started horse customisation system

---
+++ Assets/LukeTesting/Scripts/UI/Tab.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tab : MonoBehaviour
+{
+    public CustomisationOption _customisation;
+    public List<string> tabOption;
+    public List<Material> colorOption;
+    public List<Texture2D> texture2D;
+    public List<GameObject> modelOption;
+}
+
+public enum CustomisationOption
+{
+    CartColour,
+    HorseColour,
+    WheelColour,
+    HorseHat,
+    CartModel
+}
