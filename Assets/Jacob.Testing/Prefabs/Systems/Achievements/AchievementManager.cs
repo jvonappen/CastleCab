8a97a955c2fcb043367b8102cb5c14a1c982155d
jvonappen <jacobe.vonappen@gmail.com>
2023-12-07 10:43:15 +1030

    Why did I refactor?
    Trying to simplify the tracker code. Not very well though

--- Assets/Jacob.Testing/Prefabs/Systems/Achievements/AchievementManager.cs
+++ Assets/Jacob.Testing/Prefabs/Systems/Achievements/AchievementManager.cs
@@ -14,27 +14,14 @@ public class AchievementManager : MonoBehaviour
     /* 
 Achievements
      * Spa Day – Find the Pigs in mud baths 
-     * Elderly Citizen’s Home – Delivery grandma 
-     * SmoothCriminal - Remove max wanted level 
-  X   * Collidesdale - Break 100 Market Stalls
-  X   * BaaBoom 
-  X   * PloughHorse – Fences
-  X   * Pegasus - Do a flip
-     * ShowPony – Colour change
-  X   * Platnium Driver – Complete(x) Quests
-  X   * BBC – Find the big big chook
+     * Elderly Citizen’s Home – Deliver grandma 
     
- Collectables: 
+ Collectables:  still missing
      * Letters [Castle Cab] **Castle Cab
      * Chicken Men ** The Colonel
      * Scarecrows ** Hay-man
      * Jesters  ** Funny Guy
-
-     * Pigs ** Makin' Bacon
-     * Chickens ** Cluck Me!
-     * Horses ** Glue factory
-     * NPCS ** Menace
-     * Tomb stones ** Grave Digger
+     * Goblin Party
 
      */
 
@@ -46,73 +33,100 @@ Achievements
     [SerializeField] private Canvas _achievementCanvas;
     [SerializeField] private TextMeshProUGUI _achievementText;
 
-    [Header("Pegasus")]
-    public bool unlockPegasus = false;
-    public static int airTrickTracker = 0;
-    [SerializeField] private int airTricksNeeded = 5;
-    [SerializeField] private AchievementBoxDetail AT_Pegasus;
+    //Once off Achievements:
 
     [Header("SmoothCriminal")]
-    public bool unlockSmoothCriminal = false;
-
-    [Header("BaconEggs")]
-    public bool unlockBaconEggs = false;
-    private float bTimer = 5;
-    public static bool eggCheck = false;
-    public static bool baconCheck = false;
-
+    private bool unlockSmoothCriminal = false;
+    [Space]
+    [Header("BaaBoom")]
+    [SerializeField] private AchievementBoxDetail AT_BaBoom;
+    public static bool unlockBaaBoom = false;
+    [Space]
+    [Header("ShowPony")]
+    private bool unlockShowPony = false;
+    [Space]
+    [Header("BBC")]
+    [SerializeField] private AchievementBoxDetail AT_BBC;
+    private bool unlockBBC = false;
+    [Space]
+    [Space]
+    [Header("Pegasus")]
+    [SerializeField] private int airTricksNeeded = 5;
+    [SerializeField] private AchievementBoxDetail AT_Pegasus;
+    public bool unlockPegasus = false;          //Public for debug testing
+    public static int airTrickTracker = 0;
+    [Space]
     [Header("Collidesdale")]
-    public bool unlockCollidesdale = false;
-    public static int stallTracker = 0;
     [SerializeField] private int stallsNeeded = 20;
     [SerializeField] private AchievementBoxDetail AT_Collidesdale;
-
-    [Header("BaaBoom")]
-    public static bool unlockBaaBoom = false;
-
+    private bool unlockCollidesdale = false;
+    public static int stallTracker = 0;
+    [Space]
     [Header("PloughHorse")]
-    public bool unlockPloughHorse = false;
-    public static int fenceTracker = 0;
     [SerializeField] private int fencesNeeded = 50;
     [SerializeField] private AchievementBoxDetail AT_PloughHorse;
-
-    [Header("ShowPony")]
-    public bool unlockShowPony = false;
-
-    [Header("BBC")]
-    public bool unlockBBC = false;
-
+    private bool unlockPloughHorse = false;
+    public static int fenceTracker = 0;
+    [Space]
     [Header("PlatniumDriver")]
-    public bool unlockPlatniumDriver = false;
-    public static int platniumTracker = 0;
     [SerializeField] private int taxiNeeded = 10;
-
+    [SerializeField] private AchievementBoxDetail AT_PlatniumDriver;
+    private bool unlockPlatniumDriver = false;
+    public static int platniumTracker = 0;
+    [Space]
     [Header("Makin'Bacon")]
-    public bool unlockMakinBacon = false;
-    public static int pigTracker = 0;
     [SerializeField] private int pigsNeeded = 10;
-
+    [SerializeField] private AchievementBoxDetail AT_MakinBacon;
+    private bool unlockMakinBacon = false;
+    public static int pigTracker = 0;
+    [Space]
     [Header("CluckMe")]
-    public bool unlockCluckMe = false;
-    public static int cluckTracker = 0;
     [SerializeField] private int chickensNeeded = 10;
-
+    [SerializeField] private AchievementBoxDetail AT_CluckMe;
+    private bool unlockCluckMe = false;
+    public static int cluckTracker = 0;
+    [Space]
     [Header("GlueFactory")]
-    public bool unlockGlueFactory = false;
-    public static int glueTracker = 0;
     [SerializeField] private int horsesNeeded = 10;
-
-    [Header("Menace")]
-    public bool unlockMenace = false;
-    public static int menaceTracker = 0;
+    [SerializeField] private AchievementBoxDetail AT_GlueFactory;
+    private bool unlockGlueFactory = false;
+    public static int glueTracker = 0;
+    [Space]
+    [Header("PublicMenace")]
     [SerializeField] private int npcsNeeded = 10;
-
+    [SerializeField] private AchievementBoxDetail AT_PublicMenace;
+    private bool unlockMenace = false;
+    public static int menaceTracker = 0;
+    [Space]
     [Header("GraveDigger")]
-    public bool unlockGraveDigger = false;
-    public static int graveTracker = 0;
     [SerializeField] private int gravesNeeded = 10;
-
-
+    [SerializeField] private AchievementBoxDetail AT_GraveDigger;
+    private bool unlockGraveDigger = false;
+    public static int graveTracker = 0;
+    [Space]
+    [Header("SpellingBee")]
+    [SerializeField] private int lettersNeeded = 10;
+    [SerializeField] private AchievementBoxDetail AT_SpellingBee;
+    private bool unlockSpellingBee = false;
+    public static int spellingTracker = 0;
+    [Space]
+    [Header("TheColonel")]
+    [SerializeField] private int chickenmenNeeded = 10;
+    [SerializeField] private AchievementBoxDetail AT_Colonel;
+    private bool unlockColonel = false;
+    public static int chickenmanTracker = 0;
+    [Space]
+    [Header("HayMan")]
+    [SerializeField] private int scarecrowsNeeded = 10;
+    [SerializeField] private AchievementBoxDetail AT_HayMan;
+    private bool unlockHayMan = false;
+    public static int scarecrowTracker = 0;
+    [Space]
+    [Header("FunnyGuy")]
+    [SerializeField] private int jestersNeeded = 10;
+    [SerializeField] private AchievementBoxDetail AT_FunnyGuy;
+    private bool unlockFunnyGuy = false;
+    public static int jesterTracker = 0;
 
     private UITween _uiTween;
     private bool _showUI;
@@ -143,6 +157,16 @@ Achievements
         AT_Pegasus.SetTracker(airTricksNeeded);
         AT_Collidesdale.SetTracker(stallsNeeded);
         AT_PloughHorse.SetTracker(fencesNeeded);
+        AT_PlatniumDriver.SetTracker(taxiNeeded);
+        AT_MakinBacon.SetTracker(pigsNeeded);
+        AT_CluckMe.SetTracker(chickensNeeded);
+        AT_GlueFactory.SetTracker(horsesNeeded);
+        AT_PublicMenace.SetTracker(npcsNeeded);
+        AT_GraveDigger.SetTracker(gravesNeeded);
+        AT_SpellingBee.SetTracker(lettersNeeded);
+        AT_Colonel.SetTracker(chickenmenNeeded);
+        AT_HayMan.SetTracker(scarecrowsNeeded);
+        AT_FunnyGuy.SetTracker(jestersNeeded);
     }
 
     public void DisplayAchievment()
@@ -162,20 +186,38 @@ Achievements
         
     }
 
-    public void Pegasus()  
-    { /* Airtime of (5) seconds */
-        if (airTrickTracker >= airTricksNeeded && unlockPegasus == false)
+    private void DoAllTheTrackerStuff(int trackedVal, int maxVal, AchievementBoxDetail AT, string achvName, bool unlock)
+    {
+        if (trackedVal <= maxVal && unlock == false)
         {
-            unlockPegasus = true;
-            _achievementText.text = "Pegasus";
+            AT.UpdateTrackerText(trackedVal, maxVal);
+        }
+        if (trackedVal >= maxVal && unlock == false)
+        {
+            unlock = true;
+            _achievementText.text = achvName;
             DisplayAchievment();
-            AT_Pegasus._greenTick.SetActive(true);
-            AT_Pegasus.CapTracker(airTricksNeeded);
+            AT._greenTick.SetActive(true);
+            AT.CapTracker(maxVal);
+           
         }
-        if (airTrickTracker <= airTricksNeeded && unlockPegasus == false)
-        {
-            AT_Pegasus.UpdateTrackerText(airTrickTracker, airTricksNeeded);
-        }
+    }
+
+    public void Pegasus()  
+    {
+        DoAllTheTrackerStuff(airTrickTracker, airTricksNeeded, AT_Pegasus, "Pegasus", unlockPegasus);
+        //if (airTrickTracker >= airTricksNeeded && unlockPegasus == false)
+        //{
+        //    unlockPegasus = true;
+        //    _achievementText.text = "Pegasus";
+        //    DisplayAchievment();
+        //    AT_Pegasus._greenTick.SetActive(true);
+        //    AT_Pegasus.CapTracker(airTricksNeeded);
+        //}
+        //if (airTrickTracker <= airTricksNeeded && unlockPegasus == false)
+        //{
+        //    AT_Pegasus.UpdateTrackerText(airTrickTracker, airTricksNeeded);
+        //}
     }
     public void SmoothCriminal() 
     { /* Remove max wanted level */
@@ -229,7 +271,11 @@ Achievements
             unlockPlatniumDriver = true;
             _achievementText.text = "Platnium Driver";
             DisplayAchievment();
+            AT_PlatniumDriver._greenTick.SetActive(true);
+            AT_PlatniumDriver.CapTracker(taxiNeeded);
         }
+        if (platniumTracker <= taxiNeeded && unlockPlatniumDriver == false)
+        { AT_PlatniumDriver.UpdateTrackerText(platniumTracker, taxiNeeded); }
     }
 
     public void BBC()
@@ -239,6 +285,7 @@ Achievements
             unlockBBC = true;
             _achievementText.text = "B.B.C.";
             DisplayAchievment();
+            AT_BBC._greenTick.SetActive(true);
         }
     }
 
@@ -290,4 +337,6 @@ Achievements
             DisplayAchievment();
         }
     }
+
+
 }
