20bbfad3a292a59c26e80b92bdc02b3eaeba3570
Logan Baker <loganpbaker05@gmail.com>
2024-02-22 15:49:17 +1030

    implemented stamina and cleaned playerMovement

--- Assets/_Scripts/Player/Prototype/PlayerMovement.cs
+++ Assets/_Scripts/Player/Prototype/PlayerMovement.cs
@@ -15,7 +15,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Rigidbody rb;
     [SerializeField] Rigidbody wagon;
     
-    [SerializeField] ProgressBar m_boostBar;
+    [SerializeField] ProgressBar m_staminaBar;
     #endregion
 
     #region Grounded
@@ -24,7 +24,7 @@ public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] internal LayerMask m_groundLayer;
         [SerializeField] internal Transform m_raycastPoint;
-        [SerializeField] internal float m_groundRayLength;
+        [SerializeField] internal float m_groundDist;
     }
     bool m_isGrounded;
     [SerializeField] Grounded _Grounded;
@@ -55,8 +55,8 @@ public class PlayerMovement : MonoBehaviour
     [System.Serializable]
     public struct Turning
     {
-        [SerializeField] internal float m_defaultTurnSpeed;
-        [SerializeField] internal float m_turnOnSpotSpeed, m_turnInAirSpeed;
+        [SerializeField] internal float m_defaultSpeed;
+        [SerializeField] internal float m_onSpotSpeed, m_inAirSpeed;
     }
     float m_turnInput = 0;
 
@@ -67,10 +67,10 @@ public class PlayerMovement : MonoBehaviour
     [System.Serializable]
     public struct Drifting
     {
-        [SerializeField] internal float m_driftMoveMultiplier;
-        [SerializeField] internal float m_driftMinTurnSpeed, m_driftMaxTurnSpeed, m_driftTurnAcceleration;
-        [SerializeField] internal float m_driftStrengthMultiplier;
-        [SerializeField] internal float m_driftStartCooldown;
+        [SerializeField] internal float m_moveMultiplier;
+        [SerializeField] internal float m_minTurnSpeed, m_maxTurnSpeed, m_turnAcceleration;
+        [SerializeField] internal float m_strengthMultiplier;
+        [SerializeField] internal float m_startCooldown;
     }
     float m_currentDriftTurnSpeed = 50;
     bool m_isDrifting, m_attemptingDrift;
@@ -90,28 +90,45 @@ public class PlayerMovement : MonoBehaviour
     float m_defaultWagonDrag;
     CustomDrag m_wagonDrag;
 
+    [Header("Misc")]
     [SerializeField] CartControl _CartControl;
+    #endregion
+
+    #region Stamina
+    [System.Serializable]
+    public struct Stamina
+    {
+        [SerializeField] internal float m_regenPerSec, m_regenCooldown;
+    }
+    float m_staminaRegenTimer;
+
+    [SerializeField] Stamina _Stamina;
     #endregion
 
     #region Boost
     [System.Serializable]
     public struct Boost
     {
-        [SerializeField] internal float m_maxBoostSpeed;
-        [SerializeField] internal float m_boostAccelerationRate, m_boostDecelerationRate;
-        [SerializeField] internal float m_boostCostPerSec, m_boostRegenPerSec, m_boostRegenCooldown;
+        [SerializeField] internal float m_maxSpeed;
+        [SerializeField] internal float m_accelerationRate, m_decelerationRate;
+        [SerializeField] internal float m_staminaCostPerSec;
     }
-    float m_boostRegenTimer;
     bool m_isBoosting;
 
+    [Header("Ability")]
     [SerializeField] Boost _Boost;
     #endregion
 
     #region Hurricane
-    [Header("Hurricane")]
-    [SerializeField] float m_hurricaneSpeed = 1500;
+    [System.Serializable]
+    public struct Hurricane
+    {
+        [SerializeField] internal float m_speed, m_StaminaCostPerSec;
+    }
     bool m_isHurricane;
     bool m_endingHurricane;
+
+    [SerializeField] Hurricane _Hurricane;
     #endregion
 
     #endregion
@@ -126,7 +143,7 @@ public class PlayerMovement : MonoBehaviour
 
         m_animator = GetComponentInChildren<Animator>();
 
-        if (!m_boostBar) Debug.LogWarning("Boost bar reference not found");
+        if (!m_staminaBar) Debug.LogWarning("Boost bar reference not found");
 
         #region Delegates
         m_playerInput.m_playerControls.Controls.Acceleration.performed += OnAccelerate;
@@ -158,7 +175,18 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        if (m_driftCooldownTimer < _Drifting.m_driftStartCooldown) m_driftCooldownTimer += Time.deltaTime;
+        if (m_driftCooldownTimer < _Drifting.m_startCooldown) m_driftCooldownTimer += Time.deltaTime;
+
+        // Recharge stamina
+        if (m_staminaBar & m_staminaBar.progress < 1)
+        {
+            if (m_staminaRegenTimer < _Stamina.m_regenCooldown) m_staminaRegenTimer += Time.deltaTime;
+            else
+            {
+                m_staminaBar.progress += Time.deltaTime * _Stamina.m_regenPerSec;
+                m_staminaBar.UpdateProgress();
+            }
+        }
     }
     #endregion
 
@@ -216,8 +244,6 @@ public class PlayerMovement : MonoBehaviour
     {
         m_isBoosting = true;
         if (m_turnInput != 0) SetTurnDrag();
-
-        m_boostRegenTimer = 0;
     }
 
     void OnBoostCanceled(InputAction.CallbackContext context)
@@ -277,7 +303,7 @@ public class PlayerMovement : MonoBehaviour
         m_isAccelerating = true;
 
         m_isDrifting = true;
-        m_currentDriftTurnSpeed = _Drifting.m_driftMinTurnSpeed;
+        m_currentDriftTurnSpeed = _Drifting.m_minTurnSpeed;
 
         m_driftCooldownTimer = 0;
 
@@ -307,11 +333,26 @@ public class PlayerMovement : MonoBehaviour
 
     void OnHurricaneCanceled(InputAction.CallbackContext context)
     {
-        m_endingHurricane = true;
+        if (m_isHurricane) m_endingHurricane = true;
+    }
 
-        m_cam.camSpeed = m_cam.m_originalCamSpeed;
+    void EndHurricane()
+    {
+        if (rb.transform.forward.x > prevDir.x - 2 && rb.transform.forward.x < prevDir.x + 2)
+        {
+            if (rb.transform.forward.z > prevDir.z - 2 && rb.transform.forward.z < prevDir.z + 2)
+            {
+                m_isHurricane = false;
+                m_endingHurricane = false;
 
-        m_cam.m_useOffsetOverride = false;
+                // Bug - makes player jump sometimes, maybe make player temporarily kinematic or kill velocity?
+                rb.transform.forward = prevDir;
+
+                m_cam.camSpeed = m_cam.m_originalCamSpeed;
+
+                m_cam.m_useOffsetOverride = false;
+            }
+        }
     }
 
     #endregion
@@ -326,7 +367,7 @@ public class PlayerMovement : MonoBehaviour
     private void GroundCheck()
     {
         RaycastHit hit;
-        if (Physics.Raycast(_Grounded.m_raycastPoint.position, Vector3.down, out hit, _Grounded.m_groundRayLength, _Grounded.m_groundLayer))
+        if (Physics.Raycast(_Grounded.m_raycastPoint.position, Vector3.down, out hit, _Grounded.m_groundDist, _Grounded.m_groundLayer))
         {
             if (!m_isGrounded) OnBeginGrounded();
             m_isGrounded = true;
@@ -345,39 +386,23 @@ public class PlayerMovement : MonoBehaviour
     {
         #region CalculateSpeed
 
-        // Recharge boost
-        if (m_boostBar)
+        if (m_staminaBar && m_isBoosting)
         {
-            if (!m_isBoosting)
-            {
-                if (m_boostBar.progress < 1)
-                {
-                    if (m_boostRegenTimer < _Boost.m_boostRegenCooldown) m_boostRegenTimer += Time.fixedDeltaTime;
-                    else
-                    {
-                        // Boost is recharging
-                        m_boostBar.progress += Time.fixedDeltaTime * _Boost.m_boostRegenPerSec;
-                        m_boostBar.UpdateProgress();
-                    }
-                }
-            }
-            else if (m_boostBar.progress <= 0)
+            // If stamina runs out, cancel boost
+            if (m_staminaBar.progress <= 0)
             {
                 m_isBoosting = false;
-                m_boostBar.progress = 0;
-                m_boostBar.UpdateProgress();
+                m_staminaBar.progress = 0;
+                m_staminaBar.UpdateProgress();
             }
+
+            // Reset stamina cooldown timer while boosting
+            if (m_isAccelerating) m_staminaRegenTimer = 0;
         }
         
         if (m_isAccelerating)
         {
-            if (m_isReversing)
-            {
-                // Player is reversing and accelerating simultaniously
-
-                // TODO
-            }
-            else
+            if (!m_isReversing)
             {
                 if (!m_isBoosting)
                 {
@@ -389,25 +414,25 @@ public class PlayerMovement : MonoBehaviour
                     }
                     else if (m_currentSpeed > _Speed.m_maxSpeed)
                     {
-                        m_currentSpeed -= Time.fixedDeltaTime * _Boost.m_boostDecelerationRate;
+                        m_currentSpeed -= Time.fixedDeltaTime * _Boost.m_decelerationRate;
                         if (m_currentSpeed < _Speed.m_maxSpeed) m_currentSpeed = _Speed.m_maxSpeed;
                     }
                 }
                 else
                 {
                     // Accelerate if player is boosting and isn't at max boost speed
-                    if (m_currentSpeed <= _Boost.m_maxBoostSpeed)
+                    if (m_currentSpeed <= _Boost.m_maxSpeed)
                     {
-                        if (m_currentSpeed < _Boost.m_maxBoostSpeed)
+                        if (m_currentSpeed < _Boost.m_maxSpeed)
                         {
-                            m_currentSpeed += Time.fixedDeltaTime * _Boost.m_boostAccelerationRate;
-                            if (m_currentSpeed > _Boost.m_maxBoostSpeed) m_currentSpeed = _Boost.m_maxBoostSpeed; // Caps speed at max
+                            m_currentSpeed += Time.fixedDeltaTime * _Boost.m_accelerationRate;
+                            if (m_currentSpeed > _Boost.m_maxSpeed) m_currentSpeed = _Boost.m_maxSpeed; // Caps speed at max
                         }
                         
-                        if (m_boostBar)
+                        if (m_staminaBar)
                         {
-                            m_boostBar.progress -= Time.fixedDeltaTime * _Boost.m_boostCostPerSec;
-                            m_boostBar.UpdateProgress();
+                            m_staminaBar.progress -= Time.fixedDeltaTime * _Boost.m_staminaCostPerSec;
+                            m_staminaBar.UpdateProgress();
                         }
                     }
                 }
@@ -454,7 +479,7 @@ public class PlayerMovement : MonoBehaviour
                     if (m_driftTurnInput > 0) dir -= rb.transform.right;
                     else if (m_driftTurnInput < 0) dir += rb.transform.right;
 
-                    dir *= _Drifting.m_driftMoveMultiplier;
+                    dir *= _Drifting.m_moveMultiplier;
                 }
             }
 
@@ -468,27 +493,27 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
-           rb.transform.rotation = Quaternion.Euler(rb.transform.rotation.eulerAngles + new Vector3(0f, m_hurricaneSpeed * Time.fixedDeltaTime, 0f));
-
-            float velY = rb.velocity.y;
-            rb.velocity = prevDir * m_currentSpeed;
-            rb.velocity = new Vector3(rb.velocity.x, velY, rb.velocity.z);
-
-            m_cam.transform.position += rb.velocity * Time.fixedDeltaTime;
-
-            if (m_endingHurricane)
+            if (m_staminaBar)
             {
-                if (rb.transform.forward.x > prevDir.x - 2 && rb.transform.forward.x < prevDir.x + 2)
+                float staminaCostThisFrame = _Hurricane.m_StaminaCostPerSec * Time.fixedDeltaTime;
+                if (m_staminaBar.progress >= staminaCostThisFrame)
                 {
-                    if (rb.transform.forward.z > prevDir.z - 2 && rb.transform.forward.z < prevDir.z + 2)
-                    {
-                        m_isHurricane = false;
-                        m_endingHurricane = false;
+                    m_staminaBar.progress -= staminaCostThisFrame;
+                    m_staminaBar.UpdateProgress();
 
-                        // Bug - makes player jump sometimes, maybe make player temporarily kinematic or kill velocity?
-                        rb.transform.forward = prevDir;
-                    }
+                    m_staminaRegenTimer = 0;
+
+                    rb.transform.rotation = Quaternion.Euler(rb.transform.rotation.eulerAngles + new Vector3(0f, _Hurricane.m_speed * Time.fixedDeltaTime, 0f));
+
+                    float velY = rb.velocity.y;
+                    rb.velocity = prevDir * m_currentSpeed;
+                    rb.velocity = new Vector3(rb.velocity.x, velY, rb.velocity.z);
+
+                    m_cam.transform.position += rb.velocity * Time.fixedDeltaTime;
+
+                    if (m_endingHurricane) EndHurricane();
                 }
+                else if (m_isHurricane) EndHurricane();
             }
         }
 
@@ -501,27 +526,27 @@ public class PlayerMovement : MonoBehaviour
         if (m_isHurricane) return;
 
         float turnInput = m_turnInput;
-        float turnSpeed = _Turning.m_defaultTurnSpeed;
+        float turnSpeed = _Turning.m_defaultSpeed;
         if (m_isDrifting)
         {
             turnInput = m_driftTurnInput;
 
-            if (m_driftCooldownTimer >= _Drifting.m_driftStartCooldown)
+            if (m_driftCooldownTimer >= _Drifting.m_startCooldown)
             {
-                if (m_currentDriftTurnSpeed < _Drifting.m_driftMaxTurnSpeed)
+                if (m_currentDriftTurnSpeed < _Drifting.m_maxTurnSpeed)
                 {
-                    m_currentDriftTurnSpeed += Time.fixedDeltaTime * _Drifting.m_driftTurnAcceleration;
-                    if (m_currentDriftTurnSpeed > _Drifting.m_driftMaxTurnSpeed) m_currentDriftTurnSpeed = _Drifting.m_driftMaxTurnSpeed;
+                    m_currentDriftTurnSpeed += Time.fixedDeltaTime * _Drifting.m_turnAcceleration;
+                    if (m_currentDriftTurnSpeed > _Drifting.m_maxTurnSpeed) m_currentDriftTurnSpeed = _Drifting.m_maxTurnSpeed;
                 }
             }
 
             float turnStrength = m_turnInput + 1;
             if (turnInput < 0) turnStrength = Mathf.Abs(m_turnInput - 1);
 
-            turnSpeed = m_currentDriftTurnSpeed * turnStrength * _Drifting.m_driftStrengthMultiplier;
+            turnSpeed = m_currentDriftTurnSpeed * turnStrength * _Drifting.m_strengthMultiplier;
         }
-        else if (!m_isGrounded && !m_isBoosting) turnSpeed = _Turning.m_turnInAirSpeed;
-        else if (!m_isAccelerating && !m_isReversing) turnSpeed = _Turning.m_turnOnSpotSpeed;
+        else if (!m_isGrounded && !m_isBoosting) turnSpeed = _Turning.m_inAirSpeed;
+        else if (!m_isAccelerating && !m_isReversing) turnSpeed = _Turning.m_onSpotSpeed;
 
         if (turnInput != 0) rb.transform.rotation = Quaternion.Euler(rb.transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnSpeed * Time.deltaTime, 0f));
     }
