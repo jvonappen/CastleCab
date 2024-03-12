1db00cac819bb5e3e6b981a57d506805807bd7fb
Logan Baker <loganpbaker05@gmail.com>
2024-03-12 15:46:59 +1030

    stuffed with particles

--- Assets/_Scripts/Particles/DustParticle.cs
+++ Assets/_Scripts/Particles/DustParticle.cs
@@ -1,12 +1,24 @@
+using UnityEngine;
+
 public class DustParticle : ParticleHandler
 {
+    [SerializeField] float m_minSpeedToTrigger = 5;
+
+    bool m_isActive;
+
     private void Start()
     {
-        m_playerMovement.onStartedMovement += OnStartMovement;
-        m_playerMovement.onStoppedMovement += StopParticle;
+        //m_playerMovement.onStartedMovement += OnStartMovement;
+        //m_playerMovement.onStoppedMovement += StopParticle;
 
         m_playerMovement.onGrounded += OnGrounded;
         m_playerMovement.onExitGrounded += StopParticle;
+    }
+
+    private void Update()
+    {
+        if (m_playerMovement.currentSpeed < m_minSpeedToTrigger) StopParticle();
+        else if(!m_isEmitting && m_playerMovement.isGrounded) PlayParticle();
     }
 
     void OnStartMovement()
--- Assets/_Scripts/Particles/ParticleHandler.cs
+++ Assets/_Scripts/Particles/ParticleHandler.cs
@@ -6,12 +6,22 @@ public class ParticleHandler : MonoBehaviour
     [SerializeField] protected PlayerMovement m_playerMovement;
     protected ParticleSystem m_particleSystem;
 
+    protected bool m_isEmitting;
+
     private void Awake()
     {
         m_particleSystem = GetComponent<ParticleSystem>();
         StopParticle();
     }
 
-    protected void PlayParticle() => m_particleSystem.Play();
-    protected void StopParticle() => m_particleSystem.Stop();
+    protected void PlayParticle()
+    {
+        m_particleSystem.Play();
+        m_isEmitting = true;
+    }
+    protected void StopParticle()
+    {
+        m_particleSystem.Stop();
+        m_isEmitting = false;
+    }
 }
