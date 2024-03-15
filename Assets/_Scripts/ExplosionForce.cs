5769f66f89e8a5ffa9cd4667ecb6008279f7da36
HeMan-Sigmar <99298262+HeMan-Sigmar@users.noreply.github.com>
2024-03-15 11:48:39 +1030

    Object Destruction - add if statement into explosionforce script

--- Assets/_Scripts/ExplosionForce.cs
+++ Assets/_Scripts/ExplosionForce.cs
@@ -12,6 +12,7 @@ public class ExplosionForce : MonoBehaviour
     // Start is called before the first frame update
 
     // TODO <- float Break force
+    public float breakForce = 30f;
 
     private void OnTriggerEnter(Collider other) => CheckCollision(other);
     private void OnCollisionEnter(Collision collision) => CheckCollision(collision.collider);
@@ -24,11 +25,11 @@ public class ExplosionForce : MonoBehaviour
             Debug.Log("player force = " + playerForce);
 
             // TODO <- if playerforce > breakforce
-
-            Explode();
-            Destroy(gameObject);
-
-            // end
+            if (playerForce > breakForce)
+            {
+                Explode();
+                Destroy(gameObject,1.5f);
+            }
         }
     }
 
