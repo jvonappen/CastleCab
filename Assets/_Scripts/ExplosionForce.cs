a429072bf64b55ab0f3b84ddb0eeeebe588256d8
HeMan-Sigmar <99298262+HeMan-Sigmar@users.noreply.github.com>
2024-03-18 16:26:05 +1030

    ExplosionForce Refine: Horse can now be knock backed by explosion

--- Assets/_Scripts/ExplosionForce.cs
+++ Assets/_Scripts/ExplosionForce.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class ExplosionForce : MonoBehaviour
@@ -8,7 +9,7 @@ public class ExplosionForce : MonoBehaviour
     public float explosionForce = 9000f;
     public float verticalLaunchForce = 1500f; // Adjust the vertical launch force as needed.
     public float rotationForce = 500f; // Adjust the rotation force as needed.
-    // Start is called before the first frame update
+    PlayerMovement movement;
 
     // TODO <- float Break force
     public float breakForce = 30f;
@@ -25,8 +26,11 @@ public class ExplosionForce : MonoBehaviour
             // TODO <- if playerforce > breakforce
             if (playerForce > breakForce)
             {
+                other.attachedRigidbody.velocity = Vector3.zero;
+
                 Explode();
-                Destroy(gameObject,1.5f);
+
+                Destroy(gameObject);
             }
         }
     }
@@ -40,15 +44,29 @@ public class ExplosionForce : MonoBehaviour
         {
 
             // Apply an explosion force to rigidbodies.
-            Rigidbody rb = collider.GetComponentInChildren<Rigidbody>();
+            Rigidbody rb = collider.attachedRigidbody;// .GetComponentInChildren<Rigidbody>();
             if (rb != null)
             {
-                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
-                // Apply vertical launch force.
-                rb.AddForce(Vector3.up * verticalLaunchForce, ForceMode.Impulse);
 
-                Vector3 randomRotation = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-                rb.AddTorque(randomRotation * rotationForce);
+                // Calculate direction from the explosion center to the rigidbody.
+                Vector3 explosionDir = rb.transform.position - transform.position;
+
+                Debug.Log("Exploded: " + rb.transform);
+                if (rb.TryGetComponent(out Knockback knockback))
+                {
+                    knockback.KnockBack(explosionDir);
+                }
+                else
+                {
+                    rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+
+                    //Apply vertical launch force.
+                    rb.AddForce(Vector3.up * verticalLaunchForce, ForceMode.Impulse);
+
+                    Vector3 randomRotation = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+                    rb.AddTorque(randomRotation * rotationForce);
+                }
+
             }
 
 
