f2f617606042793d0ed108162ac7c50b74a2ac76
Logan Baker <loganpbaker05@gmail.com>
2024-04-04 14:16:27 +1030

    instanced materials for cosmetics

--- Assets/_Scripts/ModelSettings.cs
+++ Assets/_Scripts/ModelSettings.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class ModelSettings : MonoBehaviour
 {
-    [SerializeField] bool m_useBaseColour;
-    [ConditionalHide("m_useBaseColour")] [SerializeField] Texture m_baseColour;
-
+    [SerializeField] Texture m_baseColour;
     [SerializeField] Texture m_maskMap;
 
-    public bool useBaseColour { get { return m_useBaseColour; } }
     public Texture baseColour { get { return m_baseColour; } }
     public Texture maskMap { get { return m_maskMap; } }
 }
