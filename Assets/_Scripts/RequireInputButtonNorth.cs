f7332344c51a3353ece8e00034c3ec2566cfedb4
Logan Baker <loganpbaker05@gmail.com>
2024-04-04 15:14:25 +1030

    I AM A GOD AMONGST MEN - Fixed aspect ratios and fixed UI input while rotating display

--- Assets/_Scripts/RequireInputButtonNorth.cs
+++ Assets/_Scripts/RequireInputButtonNorth.cs
@@ -8,12 +8,14 @@ public class RequireInputButtonNorth : MonoBehaviour
     [SerializeField] PlayerInputHandler m_playerInput;
     [SerializeField] MonoBehaviour m_scriptToRequire;
 
+    [SerializeField] bool m_isInverse;
+
     private void Start()
     {
         m_playerInput.m_playerControls.Controls.StatsMenu.performed += OnPerformed;
         m_playerInput.m_playerControls.Controls.StatsMenu.canceled += OnCanceled;
     }
 
-    void OnPerformed(InputAction.CallbackContext context) => m_scriptToRequire.enabled = true;
-    void OnCanceled(InputAction.CallbackContext context) => m_scriptToRequire.enabled = false;
+    void OnPerformed(InputAction.CallbackContext context) => m_scriptToRequire.enabled = !m_isInverse;
+    void OnCanceled(InputAction.CallbackContext context) => m_scriptToRequire.enabled = m_isInverse;
 }
