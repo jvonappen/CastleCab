a767efd86c22ed7e1333d9ae480be237b2cb6ce3
Logan Baker <loganpbaker05@gmail.com>
2024-05-01 15:38:24 +0930

    added model prefab, dye system, arena load

--- Assets/_Scripts/Customisation/ColourSelector.cs
+++ Assets/_Scripts/Customisation/ColourSelector.cs
@@ -12,23 +12,9 @@ public class ColourSelector : MonoBehaviour
     ModelSelector m_modelSelector;
     private void Awake() => m_modelSelector = GetComponent<ModelSelector>();
 
-    public void SetMainDye(SO_Dye _dye)
-    {
-        SetDye("Main", _dye);
-        m_mainDye = _dye;
-    }
-
-    public void SetSecondaryDye(SO_Dye _dye)
-    {
-        SetDye("Secondary", _dye);
-        m_secondaryDye = _dye;
-    }
-
-    public void SetTertiaryDye(SO_Dye _dye)
-    {
-        SetDye("Tertiary", _dye);
-        m_tertiaryDye = _dye;
-    }
+    public void SetMainDye(SO_Dye _dye) => SetDye("Main", _dye);
+    public void SetSecondaryDye(SO_Dye _dye) => SetDye("Secondary", _dye);
+    public void SetTertiaryDye(SO_Dye _dye) => SetDye("Tertiary", _dye);
 
     public void ResetMainDye() => ResetDye("Main");
     public void ResetSecondaryDye() => ResetDye("Secondary");
@@ -41,14 +27,23 @@ public class ColourSelector : MonoBehaviour
             SetDye(_colourSegment, settings.defaultMat.GetColor("_" + _colourSegment + "_Colour"), settings.defaultMat.GetFloat("_" + _colourSegment + "_Metal"), settings.defaultMat.GetFloat("_" + _colourSegment + "_Rough"));
     }
 
-    void SetDye(string _colourSegment, SO_Dye _dye) => SetDye(_colourSegment, _dye.m_colour, _dye.m_metal, _dye.m_roughness);
+    public void SetDye(string _colourSegment, SO_Dye _dye)
+    {
+        if (_colourSegment == "Main") m_mainDye = _dye;
+        else if (_colourSegment == "Secondary") m_secondaryDye = _dye;
+        else if (_colourSegment == "Tertiary") m_tertiaryDye = _dye;
+
+        SetDye(_colourSegment, _dye.m_colour, _dye.m_metal, _dye.m_roughness);
+    }
 
     void SetDye(string _colourSegment, Color _colour, float _metal, float _roughness)
     {
         Material mat = m_modelSelector.GetMat();
-
-        mat.SetColor("_" + _colourSegment + "_Colour", _colour);
-        mat.SetFloat("_" + _colourSegment + "_Metal", _metal);
-        mat.SetFloat("_" + _colourSegment + "_Rough", _roughness);
+        if (mat)
+        {
+            mat.SetColor("_" + _colourSegment + "_Colour", _colour);
+            mat.SetFloat("_" + _colourSegment + "_Metal", _metal);
+            mat.SetFloat("_" + _colourSegment + "_Rough", _roughness);
+        }
     }
 }
--- Assets/_Scripts/ReadyUp.cs
+++ Assets/_Scripts/ReadyUp.cs
@@ -6,12 +6,14 @@ using UnityEngine.InputSystem;
 public class ReadyUp : MonoBehaviour
 {
     [SerializeField] PlayerInputHandler m_input;
+    //[SerializeField] string m_sceneToLoad;
 
     private void OnEnable() => m_input.m_playerControls.Controls.Boost.performed += StartGame;
     private void OnDisable() => m_input.m_playerControls.Controls.Boost.performed -= StartGame;
 
     void StartGame(InputAction.CallbackContext context)
     {
-        GameManager.Instance.LoadScene("MasterScene");
+        GameManager.Instance.LoadScene(GameManager.Instance.GetComponent<SceneToLoad>().sceneToLoad);
+        //GameManager.Instance.LoadScene(m_sceneToLoad);
     }
 }
