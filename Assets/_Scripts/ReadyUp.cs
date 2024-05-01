34d28abccc95733e5be5a869d7aff4be9e876fd0
Logan Baker <loganpbaker05@gmail.com>
2024-05-01 15:50:15 +0930

    Cosmetics fixed (For real)

--- Assets/_Scripts/ReadyUp.cs
+++ Assets/_Scripts/ReadyUp.cs
@@ -6,14 +6,14 @@ using UnityEngine.InputSystem;
 public class ReadyUp : MonoBehaviour
 {
     [SerializeField] PlayerInputHandler m_input;
-    //[SerializeField] string m_sceneToLoad;
 
     private void OnEnable() => m_input.m_playerControls.Controls.Boost.performed += StartGame;
     private void OnDisable() => m_input.m_playerControls.Controls.Boost.performed -= StartGame;
 
     void StartGame(InputAction.CallbackContext context)
     {
+        foreach (PlayerCustomization player in FindObjectsOfType<PlayerCustomization>()) player.StoreCustomizationsToPlayer();
+
         GameManager.Instance.LoadScene(GameManager.Instance.GetComponent<SceneToLoad>().sceneToLoad);
-        //GameManager.Instance.LoadScene(m_sceneToLoad);
     }
 }
