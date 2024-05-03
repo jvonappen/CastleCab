09ce90970a979e5a7a5bfecef5735c774987fbdc
Logan Baker <loganpbaker05@gmail.com>
2024-05-03 11:18:44 +0930

    Added UI for menu and customization and fixed character controller physics

--- Assets/_Scripts/CustomGravity.cs
+++ Assets/_Scripts/CustomGravity.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class CustomGravity : MonoBehaviour
 {
+    [SerializeField] PlayerMovement m_movement;
+
     [SerializeField] Rigidbody rb;
     [SerializeField] float m_downForce;
 
-    private void FixedUpdate() =>
+    private void FixedUpdate()
+    {
+        if (m_movement.isGrounded)
         rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y - (m_downForce * Time.fixedDeltaTime), rb.velocity.z);
+    }
+        
 }
