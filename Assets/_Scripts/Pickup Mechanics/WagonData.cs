ed6f3a22050a012c77c28e3485ae8a496453bc68
jvonappen <99298095+jvonappen@users.noreply.github.com>
2024-05-14 11:18:13 +0930

    Adjusting Rank system

--- Assets/_Scripts/Pickup Mechanics/WagonData.cs
+++ Assets/_Scripts/Pickup Mechanics/WagonData.cs
@@ -24,14 +24,14 @@ public class WagonData : MonoBehaviour
         wagonSlot = wagonSlotPoint;
         playerNumber = playerNumber + 1;
         thisPlayerNumber = playerNumber;
-        //AddToRankSystem();
+        AddToRankSystem();
         r1st.SetActive(false); r2nd.SetActive(false); r3rd.SetActive(false); r4th.SetActive(false);
         MapScreenLocatioMoverPerPlayerJoined.Instance.MapPosUpdate();
     }
 
     private void AddToRankSystem()
     {
-        if(playerNumber  == 1) { RankingSystem.Instance.P1 = this; }
+        if (playerNumber == 1) { RankingSystem.Instance.P1 = this; }
         if (playerNumber == 2) { RankingSystem.Instance.P2 = this; }
         if (playerNumber == 3) { RankingSystem.Instance.P3 = this; }
         if (playerNumber == 4) { RankingSystem.Instance.P4 = this; }
--- Assets/_Scripts/RankingSystem.cs
+++ Assets/_Scripts/RankingSystem.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class RankingSystem : MonoBehaviour
 {
     public static RankingSystem Instance;
-
     [SerializeField] public WagonData P1, P2, P3, P4;
-    [SerializeField] private int scoreP1, scoreP2, scoreP3, scoreP4;
 
+    [Header("Ranking Position UI")]
+    [SerializeField] GameObject first;
+
+    [Header("Debug")]
+    [SerializeField] private int scoreP1, scoreP2, scoreP3, scoreP4;
+    private int rank1, rank2, rank3, rank4;
     
 
     private void Awake()
@@ -19,61 +23,55 @@ public class RankingSystem : MonoBehaviour
             gameObject.transform.SetParent(null);
             DontDestroyOnLoad(gameObject);
         }
-        else
-        {
-            Destroy(gameObject);
-        }
-
-        
-       
+        else { Destroy(gameObject); }          
     }
 
-    public void UpdateRank(int score, WagonData player)
-    {
-        PlayerRank(score, player);
+    //public void UpdateRank(int score, WagonData player)
+    //{
+    //    PlayerRank(score, player);
 
-        if (P1 != null) { PlayerRank(scoreP1, P1); DoTheRank(P1); }
-        if (P2 != null) { PlayerRank(scoreP2, P2); DoTheRank(P2); }
-        if (P3 != null) { PlayerRank(scoreP3, P3); DoTheRank(P3); }
-        if (P4 != null) { PlayerRank(scoreP4, P4); DoTheRank(P4); }
+    //    if (P1 != null) { PlayerRank(scoreP1, P1); DoTheRank(P1); }
+    //    if (P2 != null) { PlayerRank(scoreP2, P2); DoTheRank(P2); }
+    //    if (P3 != null) { PlayerRank(scoreP3, P3); DoTheRank(P3); }
+    //    if (P4 != null) { PlayerRank(scoreP4, P4); DoTheRank(P4); }
 
-    }
+    //}
 
-    private void PlayerRank(int score, WagonData player)
-    {
+    //private void PlayerRank(int score, WagonData player)
+    //{
 
-        int rank = 0;
+    //    int rank = 0;
 
-        if (P1 != null && score > scoreP1) rank = +1;
-        if (P2 != null && score > scoreP2) rank = +1;
-        if (P3 != null && score > scoreP3) rank = +1;
-        if (P4 != null && score > scoreP4) rank = +1;
+    //    if (P1 != null && score > scoreP1) rank = +1;
+    //    if (P2 != null && score > scoreP2) rank = +1;
+    //    if (P3 != null && score > scoreP3) rank = +1;
+    //    if (P4 != null && score > scoreP4) rank = +1;
 
-        if (P1 != null && score < scoreP1) rank = -1;
-        if (P2 != null && score < scoreP2) rank = -1;
-        if (P3 != null && score < scoreP3) rank = -1;
-        if (P4 != null && score < scoreP4) rank = -1;
+    //    if (P1 != null && score < scoreP1) rank = -1;
+    //    if (P2 != null && score < scoreP2) rank = -1;
+    //    if (P3 != null && score < scoreP3) rank = -1;
+    //    if (P4 != null && score < scoreP4) rank = -1;
 
-        player.rank = rank;
+    //    player.rank = rank;
 
-    }
+    //}
 
-    private void DoTheRank(WagonData player)
-    {
-        ResetRankIcons(player);
-        if (player.rank == 1) player.r1st.SetActive(true);
-        if (player.rank == 2) player.r2nd.SetActive(true);
-        if (player.rank == 3) player.r3rd.SetActive(true);
-        if (player.rank == 4) player.r4th.SetActive(true);
-    }
+    //private void DoTheRank(WagonData player)
+    //{
+    //    ResetRankIcons(player);
+    //    if (player.rank == 1) player.r1st.SetActive(true);
+    //    if (player.rank == 2) player.r2nd.SetActive(true);
+    //    if (player.rank == 3) player.r3rd.SetActive(true);
+    //    if (player.rank == 4) player.r4th.SetActive(true);
+    //}
 
-    private void ResetRankIcons(WagonData player)
-    {
-        player.r1st.SetActive(false);
-        player.r2nd.SetActive(false);
-        player.r3rd.SetActive(false);
-        player.r4th.SetActive(false);
+    //private void ResetRankIcons(WagonData player)
+    //{
+    //    player.r1st.SetActive(false);
+    //    player.r2nd.SetActive(false);
+    //    player.r3rd.SetActive(false);
+    //    player.r4th.SetActive(false);
 
-    }
+    //}
 
 }
