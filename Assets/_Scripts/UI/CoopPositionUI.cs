4808a5cb5b6889cc574cf7d70752aabc02d796ed
Logan Baker <loganpbaker05@gmail.com>
2024-05-14 10:14:15 +0930

    fixed rectTransform destroyed error

--- Assets/_Scripts/UI/CoopPositionUI.cs
+++ Assets/_Scripts/UI/CoopPositionUI.cs
@@ -23,8 +23,11 @@ public class CoopPositionUI : PlayerJoinedNotifier
     {
         if (m_playerInputManager)
         {
-            if (m_playerInputManager.playerCount > 1) m_transform.anchoredPosition = new Vector3((Screen.width / 2) * m_coopPos.x, (Screen.height / 2) * m_coopPos.y, m_coopPos.z);
-            else m_transform.anchoredPosition = m_originalPos;
+            if (m_transform)
+            {
+                if (m_playerInputManager.playerCount > 1) m_transform.anchoredPosition = new Vector3((Screen.width / 2) * m_coopPos.x, (Screen.height / 2) * m_coopPos.y, m_coopPos.z);
+                else m_transform.anchoredPosition = m_originalPos;
+            }
         }
     }
 
