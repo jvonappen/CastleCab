f96c65236081ba3d8754b7169fa327073c6165fb
Logan Baker <loganpbaker05@gmail.com>
2024-05-15 09:44:58 +0930

    Fixed error when closing game

--- Assets/_Scripts/PlayerJoinedNotifier.cs
+++ Assets/_Scripts/PlayerJoinedNotifier.cs
@@ -6,13 +6,20 @@ public class PlayerJoinedNotifier : MonoBehaviour
 {
     protected PlayerInputManager m_playerInputManager;
     public virtual void Awake() => m_playerInputManager = FindObjectOfType<PlayerInputManager>();
-    public virtual void Start()
+    public virtual void OnEnable()
     {
         InputManager.onPlayerJoined += OnPlayerJoined;
         InputManager.onPlayerLeft += OnPlayerLeft;
+
+        OnPlayerUpdated();
     }
 
-    public virtual void OnEnable() => OnPlayerUpdated();
+    private void OnDisable()
+    {
+        InputManager.onPlayerJoined -= OnPlayerJoined;
+        InputManager.onPlayerLeft -= OnPlayerLeft;
+    }
+
     public virtual void OnPlayerJoined(PlayerInput _player, List<PlayerInput> _players) => OnPlayerUpdated();
     public virtual void OnPlayerLeft(PlayerInput _player, List<PlayerInput> _players) => OnPlayerUpdated();
     public virtual void OnPlayerUpdated() { }
