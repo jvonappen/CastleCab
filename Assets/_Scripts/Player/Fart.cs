ba1c432089885789a52709c7efaf0a8d87aef2c9
Logan Baker <loganpbaker05@gmail.com>
2024-05-24 14:56:55 +0930

    Added core functionality

---
+++ Assets/_Scripts/Player/Fart.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInputHandler))]
+public class Fart : MonoBehaviour
+{
+    [SerializeField] ParticleSystem m_fart, m_megaFart;
+
+    PlayerInputHandler m_inputHandler;
+    private void Awake() => m_inputHandler = GetComponent<PlayerInputHandler>();
+
+    private void OnEnable()
+    {
+        m_inputHandler.m_playerControls.Controls.Fart.performed += NormalFart;
+        m_inputHandler.m_playerControls.Controls.MegaFart.performed += MegaFart;
+    }
+
+    private void OnDisable()
+    {
+        m_inputHandler.m_playerControls.Controls.Fart.performed -= NormalFart;
+        m_inputHandler.m_playerControls.Controls.MegaFart.performed -= MegaFart;
+    }
+
+    void NormalFart(InputAction.CallbackContext context) => NormalFart();
+    void MegaFart(InputAction.CallbackContext context) => MegaFart();
+
+    public void NormalFart()
+    {
+        m_fart.gameObject.SetActive(true);
+        m_fart.Play();
+        AudioManager.Instance.PlayGroupAudio("Farts");
+    }
+
+    public void MegaFart()
+    {
+        m_megaFart.gameObject.SetActive(true);
+        m_megaFart.Play();
+        AudioManager.Instance.PlayGroupAudio("Farts");
+    }
+}
