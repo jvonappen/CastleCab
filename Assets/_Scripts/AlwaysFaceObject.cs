8b4353ff2e17e07e4844fc0ff4db8e38573a8634
Logan Baker <loganpbaker05@gmail.com>
2024-06-03 14:57:27 +0930

    Added player names!

--- Assets/_Scripts/AlwaysFaceObject.cs
+++ Assets/_Scripts/AlwaysFaceObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public enum TargetType
 {
@@ -12,15 +13,27 @@ public class AlwaysFaceObject : MonoBehaviour
 {
     [SerializeField] TargetType m_targetType;
     [SerializeField] [ConditionalEnumHide("m_targetType", 1)] Transform m_target;
-    [SerializeField] bool m_useDirection, m_isInverse, m_onUpdate;
+    [SerializeField] bool m_useDirection, m_isInverse, m_onUpdate, m_updatePerCam;
+
+    //private void OnWillRenderObject()
+    //{
+    //    if (m_updatePerCam)
+    //    {
+    //        if (Camera.current && Camera.current.transform)
+    //        {
+    //            m_target = Camera.current.transform;
+    //            UpdateRotation();
+    //        }
+    //    }
+    //}
 
     private void Awake()
     {
-        m_target = Camera.main.transform;
+        if (m_targetType == TargetType.MainCamera) m_target = Camera.main.transform;
 
-        UpdateRotation();
+        if (m_target) UpdateRotation();
     }
-    private void Update() { if (m_onUpdate) UpdateRotation(); }
+    private void Update() { if (m_onUpdate && !m_updatePerCam) UpdateRotation(); }
 
     void UpdateRotation()
     {
@@ -28,5 +41,24 @@ public class AlwaysFaceObject : MonoBehaviour
         else transform.forward = m_target.forward;
 
         if (m_isInverse) transform.forward = -transform.forward;
+    }
+
+    private void OnEnable()
+    {
+        if (m_updatePerCam) RenderPipelineManager.beginCameraRendering += OnBeginCameraRendering;
+    }
+
+    private void OnDisable()
+    {
+        if (m_updatePerCam) RenderPipelineManager.beginCameraRendering -= OnBeginCameraRendering;
+    }
+
+    void OnBeginCameraRendering(ScriptableRenderContext _context, Camera _camera)
+    {
+        if (_camera.name == "Camera")
+        {
+            m_target = _camera.transform;
+            UpdateRotation();
+        }
     }
 }
