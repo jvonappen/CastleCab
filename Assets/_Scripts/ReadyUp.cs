588b8cd4bbbe8a397e426eed83c587165fad421d
Logan Baker <loganpbaker05@gmail.com>
2024-06-03 13:58:32 +0930

    See description
    - fixed customisation background mats
    - added back button in customisation menu
    fixed cart rotation on ready up and upgrade menu

--- Assets/_Scripts/ReadyUp.cs
+++ Assets/_Scripts/ReadyUp.cs
@@ -5,29 +5,48 @@ public class ReadyUp : MonoBehaviour
 {
     [SerializeField] PlayerInputHandler m_input;
     [SerializeField] SwitchUI2P m_switchUI2p;
-
+    
     [SerializeField] GameObject m_readyText, m_notReadyText;
     bool m_isReady;
+
+    [SerializeField] GameObject m_backText;
 
     private void OnEnable()
     {
         m_input.m_playerControls.UI.Submit.performed += SetReady;
         m_input.m_playerControls.UI.Cancel.performed += CancelReady;
+
+        m_input.m_playerControls.UI.Cancel.performed += Previous;
     }
     private void OnDisable()
     {
         m_input.m_playerControls.UI.Submit.performed -= SetReady;
         m_input.m_playerControls.UI.Cancel.performed -= CancelReady;
+
+        m_input.m_playerControls.UI.Cancel.performed -= Previous;
+    }
+
+    void Previous(InputAction.CallbackContext context)
+    {
+        GetComponent<ChangeMenu>().PreviousMenu();
     }
 
     void SetReady(InputAction.CallbackContext context)
     {
+        m_input.m_playerControls.UI.Cancel.performed -= Previous;
+
+        m_backText.SetActive(false);
+
         m_notReadyText.SetActive(false);
         m_readyText.SetActive(true);
         SetReady(true);
     }
     void CancelReady(InputAction.CallbackContext context)
     {
+        m_input.m_playerControls.UI.Cancel.performed += Previous;
+
+        m_backText.SetActive(true);
+
         m_readyText.SetActive(false);
         m_notReadyText.SetActive(true);
         SetReady(false);
