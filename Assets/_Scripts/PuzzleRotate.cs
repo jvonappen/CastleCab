16eafd66d30f19ec1f33c718738029cc4e5b934e
jvonappen <99298095+jvonappen@users.noreply.github.com>
2024-06-11 16:47:19 +0930

    Puzzle Loop with a Stop

--- Assets/_Scripts/PuzzleRotate.cs
+++ Assets/_Scripts/PuzzleRotate.cs
@@ -7,7 +7,7 @@ public class PuzzleRotate : MonoBehaviour
 {
     private GameObject rotatingItem;
 
-    [SerializeField] private float m_rotationValue;
+    [SerializeField] private float m_rotationPoint;
     [SerializeField] private float m_rotationSpeed;
     [SerializeField] private float m_waitTime;
 
@@ -19,9 +19,10 @@ public class PuzzleRotate : MonoBehaviour
 
     [SerializeField] private bool m_backAndForthLoop;
     [SerializeField] private bool m_continuousLoop;
+    [SerializeField] private bool m_continuousLoopWithAStop;
 
-    [SerializeField] private bool m_onHitBackAndForth;
-    [SerializeField] private bool m_onHitContinuous;
+    [SerializeField] private bool m_onInteractBackAndForth;
+    [SerializeField] private bool m_onInteractContinuous;
 
     [Header("Debug")]
     [SerializeField] private float m_currentTransRot;
@@ -32,47 +33,40 @@ public class PuzzleRotate : MonoBehaviour
     {
         rotatingItem = this.gameObject;
         m_rotationReturnPoint = transform.eulerAngles.y;
-
     }
 
     void Update()
     {
         m_currentTransRot = transform.eulerAngles.y;
+
         BackAndForthLoop();
+        ContinuousLoop();
+        ContinuousLoopWithStop();
     }
 
     private void BackAndForthLoop()
     {
         if (!m_backAndForthLoop) return;
+        if(m_currentTransRot <= m_rotationPoint && !m_rotateBack && m_rotateForth == true) { transform.Rotate(0, m_rotationSpeed * Time.deltaTime, 0, Space.Self);}
+        if(m_currentTransRot >= m_rotationPoint && !m_rotateBack)
+        { m_rotateForth = false; TimerManager.RunAfterTime(() => { m_rotateBack = true; }, m_waitTime); }
 
-        if(m_currentTransRot <= m_rotationValue && !m_rotateBack && m_rotateForth == true)
-        {              
-                transform.Rotate(0, m_rotationSpeed * Time.deltaTime, 0, Space.Self);
-        }
-
-        if(m_currentTransRot >= m_rotationValue && !m_rotateBack)
-        {    
-            m_rotateForth = false;
-            TimerManager.RunAfterTime(() =>
-            {
-                m_rotateBack = true;
-            }, m_waitTime);
-        }
-
-        if(m_rotateBack == true)
-        {
-                transform.Rotate(0, -m_rotationSpeed * Time.deltaTime, 0, Space.Self);
-                Debug.Log("Returning to Point");          
-        }
+        if(m_rotateBack == true)  { transform.Rotate(0, -m_rotationSpeed * Time.deltaTime, 0, Space.Self); }
         if(m_rotateBack == true && m_currentTransRot <= m_rotationReturnPoint)
-        {
-            m_rotateBack = false;
-            TimerManager.RunAfterTime(() =>
-            {
-                m_rotateForth = true;
-            }, m_waitTime);
-        }
-        
+        { m_rotateBack = false; TimerManager.RunAfterTime(() => { m_rotateForth = true; }, m_waitTime);}        
     }
 
+    private void ContinuousLoop()
+    {
+        if(!m_continuousLoop) return;
+        transform.Rotate(0, m_rotationSpeed * Time.deltaTime, 0, Space.Self);
+    }
+
+    private void ContinuousLoopWithStop()
+    {
+        if (!m_continuousLoopWithAStop) return;
+        if (m_currentTransRot != m_rotationPoint && m_rotateForth) { transform.Rotate(0, m_rotationSpeed * Time.deltaTime, 0, Space.Self); }
+        if(m_currentTransRot <= m_rotationPoint) { m_rotateBack = true; }
+        if(m_currentTransRot >= m_rotationPoint && m_rotateBack) { m_rotateForth = false; m_rotateBack = false; TimerManager.RunAfterTime(() => { m_rotateForth = true; }, m_waitTime); }
+    }
 }
