51a337f032adc223483b96d62713e221e09dc3f6
Logan Baker <loganpbaker05@gmail.com>
2024-06-13 12:10:31 +0930

    added moving platform parenting (for crane)

--- Assets/_Scripts/ParentOnCollision.cs
+++ Assets/_Scripts/ParentOnCollision.cs
@@ -4,5 +4,40 @@ using UnityEngine;
 
 public class ParentOnCollision : MonoBehaviour
 {
-    
+    List<GameObject> m_collidingObjects = new();
+
+    [SerializeField] bool m_useColliderParent;
+    [SerializeField] LayerMask m_collisionLayers;
+
+    private void OnTriggerEnter(Collider other) => HandleCollision(other, true);
+    private void OnTriggerExit(Collider other) => HandleCollision(other, false);
+
+    void HandleCollision(Collider _collider, bool _isEnter)
+    {
+        Rigidbody rb = _collider.attachedRigidbody;
+        if (rb)
+        {
+            GameObject go;
+
+            if (m_useColliderParent) go = rb.transform.parent.gameObject;
+            else go = rb.gameObject;
+
+            if (go)
+            {
+                if (m_collisionLayers == (m_collisionLayers | (1 << go.layer)))
+                {
+                    if (_isEnter)
+                    {
+                        Debug.Log("Parented");
+                        go.transform.parent = transform;
+                    }
+                    else
+                    {
+                        Debug.Log("Unparented");
+                        go.transform.parent = null;
+                    }
+                }
+            }
+        }
+    }
 }
