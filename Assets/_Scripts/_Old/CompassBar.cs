e04f01247f328e64a600ca8ea45491dcfc2dddf3
Logan Baker <loganpbaker05@gmail.com>
2024-06-24 15:52:48 +0930

    Added skibidi compass - with player directions

--- Assets/_Scripts/_Old/CompassBar.cs
+++ Assets/_Scripts/_Old/CompassBar.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
-public class CompassBar : MonoBehaviour
+public class CompassBar : PlayerJoinedNotifier
 {
     [SerializeField] private RectTransform compassTransform;
     [SerializeField] private RectTransform objectiveMarkerTransform;
@@ -23,10 +24,17 @@ public class CompassBar : MonoBehaviour
     [SerializeField] private GameObject objectiveMarkerImage;
     //[Header("Debug")]
 
-    private void Start()
-    {
-        
-    }
+    [Header("Players")]
+    [SerializeField] GameObject player;
+    [SerializeField] private RectTransform player1;
+    [SerializeField] private RectTransform player2;
+    [SerializeField] private RectTransform player3;
+    [SerializeField] private RectTransform player4;
+
+    Transform player1Transform;
+    Transform player2Transform;
+    Transform player3Transform;
+    Transform player4Transform;
 
     void Update()
     {
@@ -48,6 +56,59 @@ public class CompassBar : MonoBehaviour
         {
             objectiveMarkerImage.SetActive(true);
             SetMarkerPosition(objectiveMarkerTransform, objectiveObjectTransform.position);
+        }
+
+        if (player1.gameObject.activeSelf && player1Transform) SetMarkerPosition(player1, player1Transform.position);
+        if (player2.gameObject.activeSelf && player2Transform) SetMarkerPosition(player2, player2Transform.position);
+        if (player3.gameObject.activeSelf && player3Transform) SetMarkerPosition(player3, player3Transform.position);
+        if (player4.gameObject.activeSelf && player4Transform) SetMarkerPosition(player4, player4Transform.position);
+    }
+
+    public override void OnPlayerUpdated()
+    {
+        TimerManager.RunAfterTime(() =>
+        {
+            List<PlayerData> players = GameManager.Instance.players;
+            for (int i = 0; i < players.Count; i++)
+            {
+                players[i].player.GetComponentInChildren<CompassBar>().UpdatePlayerTransforms();
+            }
+        }, 0.1f);
+    }
+    public void UpdatePlayerTransforms()
+    {
+        player1.gameObject.SetActive(false);
+        player2.gameObject.SetActive(false);
+        player3.gameObject.SetActive(false);
+        player4.gameObject.SetActive(false);
+
+        List<PlayerData> players = GameManager.Instance.players;
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (players[i].player == player) continue;
+
+            int playerNum = players[i].player.GetComponent<PlayerInput>().user.index + 1;
+            Transform targetTransform = players[i].player.GetComponent<PlayerMovement>().rb.transform;
+            if (playerNum == 1)
+            {
+                player1.gameObject.SetActive(true);
+                player1Transform = targetTransform;
+            }
+            else if (playerNum == 2)
+            {
+                player2.gameObject.SetActive(true);
+                player2Transform = targetTransform;
+            }
+            else if (playerNum == 3)
+            {
+                player3.gameObject.SetActive(true);
+                player3Transform = targetTransform;
+            }
+            else if (playerNum == 4)
+            {
+                player4.gameObject.SetActive(true);
+                player4Transform = targetTransform;
+            }
         }
     }
 
