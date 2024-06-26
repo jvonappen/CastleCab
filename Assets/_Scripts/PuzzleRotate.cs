bd49acfa3307b210fcb6cb671ce36d9715ae1021
jvonappen <99298095+jvonappen@users.noreply.github.com>
2024-06-26 10:49:53 +0930

    Fixed Lumber Yard Cranes

--- Assets/_Scripts/PuzzleRotate.cs
+++ Assets/_Scripts/PuzzleRotate.cs
@@ -14,56 +14,61 @@ public class PuzzleRotate : MonoBehaviour
     }
     [SerializeField] Mode m_mode;
 
-    [SerializeField] private float m_rotationPoint;
+    [SerializeField] private float m_rotationEndPoint;
+    [SerializeField] private float m_rotationReturnPoint = 10;
     [SerializeField] private float m_rotationSpeed;
     [SerializeField] [ConditionalEnumHide("m_mode", 2)] private float m_waitTime;
 
-
-    private bool m_rotateBack = false;
-    private bool m_rotateForth = true;
+    [Header("Debug")]
+    [SerializeField] private bool m_rotateBack = false;
+    [SerializeField] private bool m_rotateForth = true;
 
     [Header("Debug")]
     [SerializeField] private float m_currentTransRot;
-    [SerializeField] private float m_rotationReturnPoint;
+    
 
 
     private void Awake()
     {
-        m_rotationReturnPoint = transform.eulerAngles.y;
+        //m_rotationReturnPoint = transform.eulerAngles.y;
     }
 
     void Update()
     {
         m_currentTransRot = transform.eulerAngles.y;
 
-        BackAndForthLoop();
-        ContinuousLoop();
-        ContinuousLoopWithStop();
+        BackAndForth();
+        ContinuousRotation();
+        ContinuousRotationWithStop();
     }
 
-    private void BackAndForthLoop()
+    private void BackAndForth()
     {
         if (m_mode != Mode.BackAndForthLoop) return;
-        if(m_currentTransRot <= m_rotationPoint && !m_rotateBack && m_rotateForth == true) { transform.Rotate(0, m_rotationSpeed * Time.deltaTime, 0, Space.Self);}
-        if(m_currentTransRot >= m_rotationPoint && !m_rotateBack)
-        { m_rotateForth = false; TimerManager.RunAfterTime(() => { m_rotateBack = true; }, m_waitTime); }
+        if(m_currentTransRot <= m_rotationEndPoint && !m_rotateBack && m_rotateForth == true) { transform.Rotate(0, m_rotationSpeed * Time.deltaTime, 0, Space.Self);}
+        if(m_currentTransRot >= m_rotationEndPoint && !m_rotateBack)
+        { m_rotateForth = false; m_rotateBack = false; TimerManager.RunAfterTime(() => { m_rotateBack = true; }, m_waitTime); }
 
-        if(m_rotateBack == true)  { transform.Rotate(0, -m_rotationSpeed * Time.deltaTime, 0, Space.Self); }
+        if(m_rotateBack == true && m_currentTransRot >= m_rotationReturnPoint)  { transform.Rotate(0, -m_rotationSpeed * Time.deltaTime, 0, Space.Self); }
         if(m_rotateBack == true && m_currentTransRot <= m_rotationReturnPoint)
-        { m_rotateBack = false; TimerManager.RunAfterTime(() => { m_rotateForth = true; }, m_waitTime);}        
+        { m_rotateBack = false; m_rotateForth = false; TimerManager.RunAfterTime(() => { m_rotateForth = true; }, m_waitTime);}
+
     }
 
-    private void ContinuousLoop()
+    private void ContinuousRotation()
     {
         if(m_mode != Mode.ContinuousLoop) return;
         transform.Rotate(0, m_rotationSpeed * Time.deltaTime, 0, Space.Self);
+
     }
 
-    private void ContinuousLoopWithStop()
+    private void ContinuousRotationWithStop()
     {
         if (m_mode != Mode.ContinuousLoopWithStop) return;
-        if (m_currentTransRot != m_rotationPoint && m_rotateForth) { transform.Rotate(0, m_rotationSpeed * Time.deltaTime, 0, Space.Self); }
-        if(m_currentTransRot <= m_rotationPoint) { m_rotateBack = true; }
-        if(m_currentTransRot >= m_rotationPoint && m_rotateBack) { m_rotateForth = false; m_rotateBack = false; TimerManager.RunAfterTime(() => { m_rotateForth = true; }, m_waitTime); }
+      
+        if (m_currentTransRot != m_rotationEndPoint && m_rotateForth) { transform.Rotate(0, m_rotationSpeed * Time.deltaTime, 0, Space.Self); }
+        if(m_currentTransRot <= m_rotationEndPoint) { m_rotateBack = true; }
+        if(m_currentTransRot >= m_rotationEndPoint && m_rotateBack) { m_rotateForth = false; m_rotateBack = false; TimerManager.RunAfterTime(() => { m_rotateForth = true; }, m_waitTime); }
+
     }
 }
