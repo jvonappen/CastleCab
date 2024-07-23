14771c9d420ec3a167aad9d36f130d0cc8560534
Logan Baker <loganpbaker05@gmail.com>
2024-07-23 13:40:49 +0930

    Integrated objects destroyed statistic in health and reworked statistic tracker to only track one statistic rather than list (Unnecessary)

--- Assets/_Scripts/Statistics/MovedStatisticTracker.cs
+++ Assets/_Scripts/Statistics/MovedStatisticTracker.cs
@@ -5,20 +5,13 @@ using UnityEngine;
 public class MovedStatisticTracker : StatisticTracker
 {
     Vector3 m_lastPos;
-    float m_movedDist;
 
-    private void Awake()
-    {
-        m_lastPos = transform.position;
-    }
-
+    private void Awake() => m_lastPos = transform.position;
     private void Update()
     {
-        m_movedDist = Vector3.Distance(transform.position, m_lastPos);
+        m_valueToAdd = Vector3.Distance(transform.position, m_lastPos);
         m_lastPos = transform.position;
 
         UpdateStatistic();
     }
-
-    public override float GetValueToAdd(TrackInfo _trackInfo) => m_movedDist;
 }
