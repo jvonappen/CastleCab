4e6331c493c12ddaf98f0a75afb43407eb8d8af2
Logan Baker <loganpbaker05@gmail.com>
2024-07-23 11:57:13 +0930

    Added statistics system and implemented time played, objects destroyed(Only trees at the moment), and trees chopped

--- Assets/_Scripts/Achievement/AchievementManager.cs
+++ Assets/_Scripts/Achievement/AchievementManager.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class AchievementManager : MonoBehaviour
 {
+    public List<SO_Achievement> m_achievements;
+
+    private void Awake()
+    {
+        foreach (SO_Achievement achievement in m_achievements)
+        {
+            if (achievement.AchievementType == AchievementType.Statistic)
+            {
+
+            }
+        }
+    }
+
     //private void Awake()
     //{
     //    GameStatistics.distanceTraveled.Changed += OnValueChanged;
