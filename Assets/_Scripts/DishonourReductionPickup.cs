bf5b657b3b6850479078465c85a7ea5c832eb122
jvonappen <jacobe.vonappen@gmail.com>
2024-07-26 15:49:14 +0930

    Tweaks and Changes to Pickup Items

---
+++ Assets/_Scripts/DishonourReductionPickup.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DishonourReductionPickup : MonoBehaviour
+{
+    [SerializeField] private int healthIncrease = 1;
+
+    [SerializeField] private float spinValue = 5;
+    [SerializeField] private float respawnTime = 3;
+
+
+
+    private WagonData m_wagonData;
+    private PlayerHealth m_playerHealth;
+
+    private void Update()
+    {
+        transform.Rotate(0, spinValue, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Wagon")
+        {
+            m_wagonData = other.GetComponent<WagonData>();
+            m_wagonData.playerHealth.HealthPickupIncrease(healthIncrease);
+            AudioManager.Instance.PlaySFX("HealthPickup");
+            gameObject.SetActive(false);
+            m_wagonData.PlayPickUpParticle();
+            TimerManager.RunAfterTime(() => { gameObject.SetActive(true); }, respawnTime);
+        }
+    }
+}
